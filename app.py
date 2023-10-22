1580badda272804fceb9f14cadf80ca4e72e1208
Sakina122 <126756350+Sakina122@users.noreply.github.com>
2023-10-22 22:55:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-import streamlit as st
-from transformers import pipeline
-from PIL import Image
+import easyocr as ocr #OCR
+import streamlit as st #Web App
+from PIL import Image #Image Processing
+import numpy as np #Image Processing
+#title
+st.title("Easy OCR - Extract Text From Image")
+#subtitle
+st.markdown("## optical Character Recognition - Using 'easyocr', 'streamlit' - hosted on 🤗 Spaces")
+st.markdown("")
+#image uploader
+image = st.file_uploader(label = "Upload your image here", type=['png','jpg','jpeg'])
+@st.cache
+def load_model():
+   reader = ocr.Reader(['en'],model_storage_directory='.')
+   return reader
+reader = load_model() #load model
+if image is not None:
+    input_image = Image.open(image) #read image
+   st.image(input_image) #display image
+with st.spinner("🧁 AI is at Work! "):
 
-pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
+result = reader.readtext(np.array(input_image))
+ result_text = [] #empty list for results
+  for text in result:
+      result_text.append(text[1])
+      st.write(result_text)
+     #st.success("Here you go!")
+   st.balloons()
+else:
+ st.write("Upload an image")
+st.caption("Made with ❤ by @littlecoder")
 
-st.title("Hot Dog? Or Not?")
 
-file_name = st.file_uploader("Upload a hot dog candidate image")
 
-if file_name is not None:
-    col1, col2 = st.columns(2)
 
-    image = Image.open(file_name)
-    col1.image(image, use_column_width=True)
-    predictions = pipeline(image)
-
-    col2.header("Probabilities")
-    for p in predictions:
-        col2.subheader(f"{ p['label'] }: { round(p['score'] * 100, 1)}%")
